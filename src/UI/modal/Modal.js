28b8653ad37d2ab2a8bafb22b87291560999032a
Joey Zheng <joey.z.rp@gmail.com>
2019-11-24 10:14:19 +1000

    added modal

--- src/UI/modal/Modal.js
+++ src/UI/modal/Modal.js
@@ -1,17 +1,29 @@
 import React from 'react';
 
+import './modal.css';
+
 const Modal = props => {
     const {
         children,
         visible,
-        ...rest
+        onCloseButtonClick,
     } = props;
 
     if (!visible) return null;
 
     return (
-        <div>
-
+        <div className="modal__overlay">
+            <div className="modal__content">
+                <div className="modal__close-button-wrapper">
+                    <span
+                        onClick={onCloseButtonClick}
+                        className="modal__close-button"
+                    >
+                        x
+                    </span>
+                </div>
+                {children}
+            </div>
         </div>
     );
 };
--- src/UIDemo/UIDemo.js
+++ src/UIDemo/UIDemo.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../UI/button/Button';
 import LoadingButton from '../UI/loadingButton/LoadingButton';
+import Modal from '../UI/modal/Modal';
 
 import './UIDemo.css';
 
 const UIDemo = () => {
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
     return (
         <div>
             <h1>UI Component Demo</h1>
@@ -13,6 +16,21 @@ const UIDemo = () => {
                 <Button primary round>my button</Button>
                 <h2>Loading Button</h2>
                 <LoadingButton round isLoading>loaded</LoadingButton>
+                <h2>Modal</h2>
+                <Button
+                    onClick={() => setIsModalVisible(true)}
+                >
+                    open modal
+                </Button>
+                <Modal
+                    visible={isModalVisible}
+                    onCloseButtonClick={() => setIsModalVisible(false)}
+                >
+                    <p>I'm modal content 1</p>
+                    <p>I'm modal content 2</p>
+                    <p>I'm modal content 3</p>
+                    <p>I'm modal content 4</p>
+                </Modal>
             </div>
         </div>
     );
